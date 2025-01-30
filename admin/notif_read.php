42c934d2fb4afc28d48c9ea8527332f25a513079
kyleigma <kyleigma36@gmail.com>
2025-01-30 15:58:37 +0800

    deleted

--- admin/notif_read.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-include 'includes/conn.php';
-
-$mark_read_query = "UPDATE notifications SET is_read = 1 WHERE user_id = ?";
-$stmt = $conn->prepare($mark_read_query);
-$stmt->bind_param("i", $user_id);
-$stmt->execute();
-?>
